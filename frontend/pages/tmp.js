e97ffcb5aeecd1dff3505f1cd338f2e852da390e
Konstantin Korovkin <kkorovkin@Konstantins-MacBook-Pro.local>
2022-08-17 09:28:38 +0300

    MTD-1: Project was initiated

---
+++ frontend/pages/tmp.js
@@ -0,0 +1,11 @@
+import MainContainer from "../components/MainContainer";
+
+const Tmp = () => {
+    return (
+        <MainContainer>
+            <h1>TMP</h1>
+        </MainContainer>
+    );
+};
+
+export default Tmp;
